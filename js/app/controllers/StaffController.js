d5e13876970bf194f039013469ac27af51426d41
kellypowers <kpowers.nj@gmail.com>
2020-07-20 19:41:59 -0400

    Done.

--- js/app/controllers/StaffController.js
+++ js/app/controllers/StaffController.js
@@ -1,8 +1,11 @@
-function StaffController($scope) {
-    $scope.name = 'Steve Jobs';
-    $scope.email = 'steve@apple.com';
+// function StaffController($scope) {
+//     $scope.name = 'Steve Jobs';
+//     $scope.email = 'steve@apple.com';
+// }
+function StaffController() {
+    this.name = 'Steve Jobs';
+    this.email = 'steve@apple.com';
 }
-
 angular
     .module('app')
     .controller('StaffController', StaffController);
